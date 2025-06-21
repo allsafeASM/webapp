a49d4c26f7866b3d83bba163beb506d17fbcef7e
Abdelrahman Magdi <abdomagdi300@gmail.com>
2025-06-21 14:28:37 +0300

    [Auth] add verified_at to users
    For email verification in case user does not want to use social
    login/signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_11_124205) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_21_104645) do
   create_schema "auth"
   create_schema "extensions"
   create_schema "graphql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_11_124205) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "verified_at"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
